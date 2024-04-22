8bf817c5a678007773cb8f94cbcf006b6d1de8ad
Anirudh Tyagi <50312196+anirudh-tyagi@users.noreply.github.com>
2024-04-22 14:21:27 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,9 @@ import streamlit as st
 st.set_page_config("Breast cancer Prediction")
 
 st.header("ML project for Breast cancer prediction")
+st.markdown("What it looks like")
+image_comparison(
+    img1="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYbsrMhcf1T-86pTi3Wbl2LRleNSGKRjmxZyW3Tk0iQA&s",
+    img2="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGw38dFB45RpeUknO6ZxNRoGgfZPS6AShVh4p5t93vqAb8kFPDhxuF7pcraqvNArlPn9U&usqp=CAU",
+    
+)
