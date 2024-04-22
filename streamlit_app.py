a1ff5e4cbc6edd0426c15fd235164db8da99b48f
Anirudh Tyagi <50312196+anirudh-tyagi@users.noreply.github.com>
2024-04-22 14:15:58 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,16 +2,12 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
+import cv2
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+st.set_page_config("Breast cancer Prediction")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+
 
 num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
 num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
