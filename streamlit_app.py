453741b52cb7349e871d9189a1caa25e3f9cad1e
Anirudh Tyagi <50312196+anirudh-tyagi@users.noreply.github.com>
2024-04-22 14:22:22 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,45 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+from streamlit_image_comparison import image_comparison
 
 
-st.set_page_config("Breast cancer Prediction")
 
-st.header("ML project for Breast cancer prediction")
-st.markdown("What it looks like")
+st.set_page_config("Webb Space Telescope vs Hubble Telescope", "🔭")
+
+st.header("🔭 J. Webb Space Telescope vs Hubble Telescope")
+
+st.write("")
+"This is a reproduction of the fantastic [WebbCompare](https://www.webbcompare.com/index.html) app by [John Christensen](https://twitter.com/JohnnyC1423). It's built in Streamlit and takes only 10 lines of Python code. If you like this app, please star [John's original repo](https://github.com/JohnEdChristensen/WebbCompare)!"
+st.write("")
+
+st.markdown("### Southern Nebula")
 image_comparison(
-    img1="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYbsrMhcf1T-86pTi3Wbl2LRleNSGKRjmxZyW3Tk0iQA&s",
-    img2="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGw38dFB45RpeUknO6ZxNRoGgfZPS6AShVh4p5t93vqAb8kFPDhxuF7pcraqvNArlPn9U&usqp=CAU",
-    
+    img1="https://www.webbcompare.com/img/hubble/southern_nebula_700.jpg",
+    img2="https://www.webbcompare.com/img/webb/southern_nebula_700.jpg",
+    label1="Hubble",
+    label2="Webb",
+)
+
+
+st.markdown("### Galaxy Cluster SMACS 0723")
+image_comparison(
+    img1="https://www.webbcompare.com/img/hubble/deep_field_700.jpg",
+    img2="https://www.webbcompare.com/img/webb/deep_field_700.jpg",
+    label1="Hubble",
+    label2="Webb",
+)
+
+st.markdown("### Carina Nebula")
+image_comparison(
+    img1="https://www.webbcompare.com/img/hubble/carina_2800.png",
+    img2="https://www.webbcompare.com/img/webb/carina_2800.jpg",
+    label1="Hubble",
+    label2="Webb",
+)
+
+st.markdown("### Stephan's Quintet")
+image_comparison(
+    img1="https://www.webbcompare.com/img/hubble/stephans_quintet_2800.jpg",
+    img2="https://www.webbcompare.com/img/webb/stephans_quintet_2800.jpg",
+    label1="Hubble",
+    label2="Webb",
 )
